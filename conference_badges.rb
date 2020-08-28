87679e2d23a64d53e154cda5003574ced3cc6612
William T Cao <williamacao@gmail.com>
2020-08-28 04:31:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(Arel)
  return "Hello, my name is Arel."
 end
 
-
+ names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def batch_badge_creator(attendees)
    attendees.each do |attendee|
