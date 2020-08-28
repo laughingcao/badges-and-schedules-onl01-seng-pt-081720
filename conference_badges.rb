2ef4e4c1db79c302fbc6f1b6c0702ec011affac1
William T Cao <williamacao@gmail.com>
2020-08-28 04:38:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,23 +1,3 @@
-def badge_maker("Arel")
- return "Hello, my name is #{Arel}."
+badge_maker("Arel")
+ return "Hello, my name is Arel"
 end
-
-attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-
-def batch_badge_creator(attendees)
-   attendees.each do |attendee|
-    puts
-end
- 
-end
-
-def assign_rooms(attendees)
-  room_assignments = 0
-  attendees.collect do |attendee|
-    room_assignments += 1
-    "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
-    
-
-end
-end
-
